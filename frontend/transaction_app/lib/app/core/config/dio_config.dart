1821247f2ab7e961123e12905129a39791e2af12
victorfidelis <fideliscorrea.victor@gmail.com>
2025-05-02 07:38:22 -0300

    api settings

--- frontend/transaction_app/lib/app/core/config/dio_config.dart
+++ frontend/transaction_app/lib/app/core/config/dio_config.dart
@@ -1,13 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
+// Use [10.0.2.2] para emulador 
+// Para dispositivo físico veja o IP da máquina em que subiu a API
+const deviceApiIp = '10.0.2.2'; 
+
 class DioConfig {
   static Dio get() {
     final String baseUrl;
     if (kIsWeb) {
       baseUrl = 'http://localhost:8080';
     } else {
-      baseUrl = 'http://10.0.2.2:8080';
+      baseUrl = 'http://$deviceApiIp:8080';
     }
     final dio = Dio(
       BaseOptions(
